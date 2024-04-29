b6339249131b01ab680438447265044c0319e63a
Anaru03 <dele22428@uvg.edu.gt>
2024-04-28 22:01:53 -0600

    Secondary fixes

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-  },
-  jsx: {
-    factory: 'React.createElement',
-    fragment: 'React.Fragment',
-    jsxRuntime: 'classic',
-  },
   resolve: {
     alias: {
       '@pages': '/src/pages',
+      '@components': '/src/components',
+      '@hooks': '/src/hooks',
+      '@assets': '/src/assets',
+      '@styles': '/src/styles'
     }
   }
 })
